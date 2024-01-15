fe78396af58c932596b0a09c231498b8d5657e52
stephen-osullivan <31805109+stephen-osullivan@users.noreply.github.com>
2024-01-15 09:40:01 +0000

    added simple chainlit app

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-
+# run with chainlit run main.py -w
+import chainlit as cl
+import openai
 import os
+
 api_key = os.environ.get('OPENAI_API_KEY')
 
-print(api_key)
+@cl.on_message
+async def main(message : str):
+    await cl.Message(content=message).send()
