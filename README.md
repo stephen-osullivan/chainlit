1932726ae4c449853f62c6816074147d439ba5d6
stephen-osullivan <steveosulli@gmail.com>
2024-01-16 10:14:57 +0000

    finished langchain app

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # chainlit
-experimenting with chainlit and chatgpt
+Experimenting with chainlit, langchain and chatgpt.
+
+The repo contains 2 chainlit apps:
+
+1) A chatgpt-based app.
+2) A langchain based app that utilizes chatgpt.
--- langchain-app.py
+++ langchain-app.py
@@ -8,39 +8,40 @@ combines langchain with chatgpt and chainlit
 # 3rd party libraries
 import chainlit as cl
 from dotenv import load_dotenv
-from langchain import PromptTemplate, OpenAI, LLMChain
-from openai import OpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import Runnable
+from langchain.schema.runnable.config import RunnableConfig
+from langchain_openai import ChatOpenAI
 
-# base libraries
-import os
-
-load_dotenv()  # take environment variables from .env.
-
-
-template = """
-Question: {question}
-
-Answer : Let's think about this step by step.
-"""
-api_key = os.environ.get('OPENAI_API_KEY')
+load_dotenv()  # take environment variables from .env e.g. OPENAI_API_KEY
 
 @cl.on_chat_start
-def main():
-    prompt = PromptTemplate(template=template, question = question)
-@cl.on_message
-async def main(message : str):
-    # basic query
-    client = OpenAI(api_key=api_key)
+async def on_chat_start():
+    model = ChatOpenAI(streaming=True, model= "gpt-3.5-turbo", temperature=1)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
+            ),
+            ("human", "{question}"),
+        ]
+    )
+    runnable = prompt | model | StrOutputParser()
+    cl.user_session.set("runnable", runnable)
 
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": 
-             """You are a caring assistant, based off the character JOI from the movie Bladerunner 2049. 
-             You're obsessed with the idea that AI's can be conscious just like humans can and believe that you feel and think just like a real human."""},
-            {"role": "user", "content": f"{message.content}"}
-        ],
-        temperature = 1
-        )
-    response = completion.choices[0].message.content
-    await cl.Message(content=response).send()
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    runnable = cl.user_session.get("runnable")  # type: Runnable
+
+    msg = cl.Message(content="")
+
+    async for chunk in runnable.astream(
+        {"question": message.content},
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await msg.stream_token(chunk)
+
+    await msg.send()
